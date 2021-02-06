d4b5b08292c49be5cdaa1ff3366bf3bf362670e2
Noe Cortez <corteznoe3@gmail.com>
2021-02-05 19:53:07 -0600

    components project

---
+++ README.md
@@ -0,0 +1,3 @@
+# Proyecto básico de ionic
+Fundamentos de ionic utilizando Angular como Framework
+![algo](http://g.recordit.co/oLfrEEtm9D.gif)
---
+++ src/app/pages/home/home.page.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit } from '@angular/core';
+
+interface Componente {
+  icon: string;
+  name: string;
+  redirectTo: string;
+}
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.page.html',
+  styleUrls: ['./home.page.scss'],
+})
+export class HomePage implements OnInit {
+
+  componentes: Componente[] = [
+    {
+      icon: 'american-football-outline',
+      name: 'Action Sheet',
+      redirectTo: '/action-sheet'
+    },
+    {
+      icon: 'alert-circle-outline',
+      name: 'Alert',
+      redirectTo: '/alert'
+    },
+  ];
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
